ea04efe928166200d99054a8ee2b96e807f86ee4
itzik.grossman <itzik.grossman@otorio.io>
2018-08-13 23:01:05 +0300

    .

--- anetping.py
+++ anetping.py
@@ -3,15 +3,30 @@ import os
 import ipaddress
 import asyncio
 import logging
-
+import struct
+import fcntl
 import icmp
+
+from typing import Optional, Tuple
 
 logger = logging.getLogger(__name__)
 
-def ping(ip):
-    pass
+packet = icmp.build()
+async def ping(ip):
+    # get socket
+    # set socket non block
+    # queue writer
+    # queue reader?
+    try:
+        (rd, wr) = await create_connection()
+        #asyncio.StreamWriter.write(packet)
+        await wr.write(packet)
+        print("open")
+    except Exception as e:
+        logger.error(f'{str(e)}')
 
-class ARPRequestProtocol(asyncio.Protocol):
+
+class PingTestProtocol(asyncio.Protocol):
 
     def __init__(self, ip):
         self.ip = ip
@@ -22,8 +37,8 @@ class ARPRequestProtocol(asyncio.Protocol):
     def connection_made(self, transport):
         logger.debug('connection made')
         self.transport = transport
-        frame = self.arp_request(self.ip)
-        transport.write(frame)
+        packet = self.icmp_echo_request(self.ip)
+        transport.write(packet)
 
     def connection_lost(self, exc):
         logger.debug('connection lost')
@@ -66,38 +81,49 @@ class ARPRequestProtocol(asyncio.Protocol):
             return self.src_ip
         else:
             sock = self.transport._sock
-            interface = pack('256s', sock.getsockname()[0].encode('ascii'))
+            interface = struct.pack('256s', sock.getsockname()[0].encode('ascii'))
             info = fcntl.ioctl(sock.fileno(), SIOCGIFADDR, interface)
             self.src_ip = ipaddress.IPv4Address(info[20:24])
             return self.src_ip
 
-    def arp_request(self, ip):
-        packet = ARP(tha='00:00:00:00:00:00', tpa=ip,
-                     sha=self._get_mac_info(), spa=self._get_ip_info())
-        frame = Ethernet(dst_mac='FF:FF:FF:FF:FF:FF',
-                         src_mac=self._get_mac_info(),
-                         ethertype='ARP',
-                         payload=packet.write())
-        log.info(frame.info())
-        log.info(packet.info())
-        return frame.write()
+    def icmp_echo_request(self, ip):
+        packet = icmp.build()
+        return packet
 
 
+ICMP_MAX_RECV = 100
 
-def create_connection():
-    exceptions = []
+
+def receive(socket):
+    recPacket, ancdata, flags, addr = socket.recvmsg(ICMP_MAX_RECV)
+    # self.queue.put_nowait((timeReceived, (dataSize + 8), iphSrcIP, \
+    #                         icmpSeqNumber, iphTTL))
+    # self.queue = asyncio.Queue(loop=self.loop)
+
+
+def send(socket, dest, packet):
+    socket.sendto(packet, (dest, 0))
+
+
+async def create_connection() -> None:
     sock = None
+    loop = asyncio.get_event_loop()
     try:
         sock = socket.socket(family=socket.AF_INET,
                              type=socket.SOCK_RAW,
                              proto=socket.IPPROTO_ICMP)
         sock.setblocking(False)
-        sock.bind(('127.0.0.1', socket.SOCK_RAW))
-    except OSError as exc:
+        #if source_ip is not None:
+        #sock.bind((socket.gethostname(), socket.SOCK_RAW))
+        # reader, writer = await asyncio.open_connection(loop=asyncio.get_event_loop(), sock=sock)
+        # return reader, writer
+        loop.add_reader(sock.fileno(), receive)
+    except OSError as e:
         logger.error(f'error while attempting to bind on interface')
     finally:
         if sock is not None:
             sock.close()
+
 
 def ip_mask_to_list(ip, netmask):
     """ return list of string ip addresses in ip/netmask (including network names and broadcasts) """
@@ -105,6 +131,7 @@ def ip_mask_to_list(ip, netmask):
     net = ipaddress.IPv4Network(f'{ip}/{netmask}')
     return [str(addr) for addr in net]
 
+
 def netping(ip, netmask):
     addrs=ip_mask_to_list(ip,netmask)
-    [ping(addr) for addr in addrs]
+    #[print(addr) for addr in addrs]
--- icmp.py
+++ icmp.py
@@ -4,9 +4,43 @@ import collections
 
 ICMPHeader = collections.namedtuple('ICMPHeader', 'type code checksum')
 
-icmp_reply = 0x8
+icmp_reply = 0x0
+
+seq = 1
+
+
+def two_byte_add(a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16)
+
+
+def checksum(msg):
+    s = 0
+    for i in range(0, len(msg), 2):
+        w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+        s = two_byte_add(s, w)
+    return ~s & 0xffff
+
 
 def parse(packet: bytes) -> ICMPHeader:
     icmp_header = packet[20:28]
     type, code, checksum, p_id, sequence = struct.unpack('bbHHh', icmp_header)
     return ICMPHeader(type, code, checksum)
+
+
+def build() -> bytes:
+    global seq
+
+    type = '\x08'
+    code = '\x00'
+    csum = '\x00\x00'
+    msg_id = '\x00\x01'
+    seq = '\x00\x01'#bytes(seq + 1 % 65536)
+    data = 'abcdefghijklmnopqrstuvwabcdefg'
+
+    msg = type + code + csum + msg_id + seq + data
+
+    csum = checksum(msg)
+    header = struct.pack('!BBHHH', 8, 0, csum, 1, 1)
+    packet = header + bytes(data.encode())
+    return packet
